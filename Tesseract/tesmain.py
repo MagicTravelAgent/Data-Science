d401b60f5c0bf27977cd0f098a8b9a18dd494db9
Lucas.Puddifoot <lucas.puddifoot@student.ru.nl>
2021-04-01 14:57:40 +0200

    downloaded 100 photos and performed ocr on them

--- Tesseract/tesmain.py
+++ Tesseract/tesmain.py
@@ -7,14 +7,15 @@ import re
 import io
 import glob
 import csv
-#from improve_spelling import improve_spelling
+
+# from improve_spelling import improve_spelling
 
 
 # in order to use tesseract we must install it
 # https://medium.com/quantrium-tech/installing-and-using-tesseract-4-on-windows-10-4f7930313f82
 
 # If you don't have tesseract executable in your PATH, include the following:
-#pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # Simple image to string
@@ -33,54 +34,65 @@ import csv
 # f.close()
 
 # Entire directory of dutch text images to strings
-#image_directory = "###"
-#output_directory = "###"
-#for image_filename in glob.glob(image_directory + "*.jpeg"):
-#      try:
-#            text = pt.image_to_string(Image.open(image_filename), lang='nld')
-#            f = open(re.sub(r"^%s(.*)\.jpeg$" % (image_directory),r"%s\1.txt" % (output_directory),image_filename), "w")
-#            f.write(text)
-#            f.close()
-#      except Exception as ex:
-             #Astraea tested this with 100 images and 15 of them threw an error like this:
-             #(-11, 'Tesseract Open Source OCR Engine v4.0.0 with Leptonica Warning: Invalid resolution 0 dpi. Using 70 instead. Estimating resolution as 126 contains_unichar_id(unichar_id):Error:Assert failed:in file ../../src/ccutil/unicharset.h, line 502')
-             #Help I have no idea how to solve it
-#            print(ex)
+image_directory = "..\Lucas Testing\Images"
+output_directory = "OCR"
+print("here")
+for image_filename in glob.glob(image_directory + "\*.jpeg"):
+
+    try:
+        # ocr the file
+        text = pt.image_to_string(Image.open(image_filename), lang='nld')
+
+        # creating the file destination
+        f_dest = image_filename.replace(image_directory, output_directory)
+        f_dest = f_dest.replace(".jpeg", ".txt")
+
+        #with the created destination write the text
+        with open(f_dest, "w", encoding="utf-8") as f:
+            f.write(text)
+            f.close()
+
+    except Exception as ex:
+        print("its fucked")
+        # Astraea tested this with 100 images and 15 of them threw an error like this:
+        # (-11, 'Tesseract Open Source OCR Engine v4.0.0 with Leptonica Warning: Invalid resolution 0 dpi. Using 70 instead. Estimating resolution as 126 contains_unichar_id(unichar_id):Error:Assert failed:in file ../../src/ccutil/unicharset.h, line 502')
+        # Help I have no idea how to solve it
+        print(ex)
 
 # assuming that the text files exist we run the code to get the text out:
-with open("test_with_dutch_post.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-f.close()
+# with open("test_with_dutch_post.txt", "r", encoding="utf-8") as f:
+#     text = f.read()
+# f.close()
 
 # now the text is open we split based on two empty lines of text:
-text = text.split("\n ")
-print(len(text))
+# text = text.split("\n ")
+# print(len(text))
 
 # we check each split to see if it contains anything from the regular expression
 # so first we build the regular expression:
-reg = ".*hypothe+.*"
+# reg = ".*hypothe+.*"
 
 # create the text file for the output of the page:
-with open("advert_found.txt", "w", encoding="utf-8") as f:
-    # look through the different paragraphs and if our reg ex succeeds then we write it to the
-    # output text file
-    for i in text:
-        x = re.search(reg, i)
-        if x:
-          f.write(i)
+# with open("advert_found.txt", "w", encoding="utf-8") as f:
+#     # look through the different paragraphs and if our reg ex succeeds then we write it to the
+#     # output text file
+#     for i in text:
+#         x = re.search(reg, i)
+#         if x:
+#           f.write(i)
 
 # close the output file
-f.close()
+# f.close()
 
 # This was Astraea trying to detect ocr from 100 images, worked only for 2 of them so don't try this at home
-#text_directory = "###"
-#output_directory = "###"
-#output_file = open(output_directory + "img_ocr.csv","w")
-#csv_writer = csv.writer(output_file)
-#csv_writer.writerow(["ID_CVB2","ID_CVB","IDv2","ID","Titel","Dag","DAG","Maand","MAAND","Jaar","Type","Type_Detail","Soort Transactie","Vraag/Aanbod","Detail","Contact","Beroep","Locatie_Contact","Handelt namens","Advertentie_Opmerking","Bedragen","Bedrag_range_minimum","Bedrag_range_maximum","Reden","Onderpand","Locatie_Onderpand","Loan-to-value","Tegenpartij_Voorkeur","Aflossing","Rente","Moment","Hypotheek_Opmerking","URL","tblResultaatMetadatakey","OCR"])
-#filenames = glob.glob(text_directory + "*.txt")
-#filenames.sort()
-#for filename in filenames:
+# text_directory = "###"
+# output_directory = "###"
+# output_file = open(output_directory + "img_ocr.csv","w")
+# csv_writer = csv.writer(output_file)
+# csv_writer.writerow(["ID_CVB2","ID_CVB","IDv2","ID","Titel","Dag","DAG","Maand","MAAND","Jaar","Type","Type_Detail","Soort Transactie","Vraag/Aanbod","Detail","Contact","Beroep","Locatie_Contact","Handelt namens","Advertentie_Opmerking","Bedragen","Bedrag_range_minimum","Bedrag_range_maximum","Reden","Onderpand","Locatie_Onderpand","Loan-to-value","Tegenpartij_Voorkeur","Aflossing","Rente","Moment","Hypotheek_Opmerking","URL","tblResultaatMetadatakey","OCR"])
+# filenames = glob.glob(text_directory + "*.txt")
+# filenames.sort()
+# for filename in filenames:
 #      print("Opening %s..." % (filename))
 #      f = open(filename, "r")
 #      #f = io.open("test_with_dutch_post.txt", "r", encoding="ISO-8859-15")
@@ -142,4 +154,4 @@ f.close()
 #            match = improve_spelling(match)
 #            print(match)
 #      csv_writer.writerow(["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",match])
-#output_file.close()
+# output_file.close()
